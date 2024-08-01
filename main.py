90aa5799e0b699d8f75b54ab35d38828493338c7
Rafael Souza <rafa.nds13@gmail.com>
2024-08-01 12:42:05 -0300

    Construindo um banco de dados com a tabela de produtos, e conectando ao bando para alimentar o algoritmo genético

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 from model.algoritmo_genetico import AlgoritmoGenetico
 from model.produto import Produto
 import matplotlib.pyplot as plt
+import pymysql
 
 lista_produtos = []
-lista_produtos.append(Produto("Geladeira Dako", 0.751, 999.90))
-lista_produtos.append(Produto("Iphone 6", 0.0000899, 2911.12))
-lista_produtos.append(Produto("TV 55' ", 0.400, 4346.99))
-lista_produtos.append(Produto("TV 50' ", 0.290, 3999.90))
-lista_produtos.append(Produto("TV 42' ", 0.200, 2999.00))
-lista_produtos.append(Produto("Notebook Dell", 0.00350, 2499.90))
-lista_produtos.append(Produto("Ventilador Panasonic", 0.496, 199.90))
-lista_produtos.append(Produto("Microondas Electrolux", 0.0424, 308.66))
-lista_produtos.append(Produto("Microondas LG", 0.0544, 429.90))
-lista_produtos.append(Produto("Microondas Panasonic", 0.0319, 299.29))
-lista_produtos.append(Produto("Geladeira Brastemp", 0.635, 849.00))
-lista_produtos.append(Produto("Geladeira Consul", 0.870, 1199.89))
-lista_produtos.append(Produto("Notebook Lenovo", 0.498, 1999.90))
-lista_produtos.append(Produto("Notebook Asus", 0.527, 3999.00))
+conexao = pymysql.connect(host="localhost", user="root", passwd="123456", db="produtos")
+cursor = conexao.cursor()
+cursor.execute('select nome, espaco, valor, quantidade from produto')
+for produto in cursor:
+    for i in range(produto[3]):
+        lista_produtos.append(Produto(produto[0], produto[1], produto[2]))
+
+cursor.close()
+conexao.close()
 
 espacos = []
 valores = []
 nomes = []
-limite = 3
+limite = 10
 tamanho_populacao = 20
 taxa_mutacao = 0.05
-numero_geracoes = 100
+numero_geracoes = 200
 
 for produto in lista_produtos:
     espacos.append(produto.espaco)
@@ -34,11 +30,6 @@ for produto in lista_produtos:
 ag = AlgoritmoGenetico(tamanho_populacao)
 ag.executa(espacos, valores, limite, numero_geracoes, taxa_mutacao)
 
-print("---------- Gerações ----------")
-print("---------- Melhores ----------")
-ag.melhores()
-print("------------------------------")
-print("\n\n")
 print("---------- Melhor Resultado ----------")
 print(ag.melhor_solucao)
 ag.produtos_selecionados(ag.melhor_solucao, nomes)
@@ -46,7 +37,7 @@ print("--------------------------------------")
 resultados_valores = []
 for i in range(len(ag.lista_solucoes)):
     resultados_valores.append(ag.lista_solucoes[i].nota_avaliacao)
-    
+
 plt.plot(resultados_valores)
 plt.title("Acompanhamento das gerações")
 plt.show()
